f1fd40ed7de0c966e5f6980c6c3016027a26ec71
Roberto Guerra <uris77@gmail.com>
2016-05-03 22:14:50 -0600

    First solution to card-game-war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,70 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn deal-cards
+  [c]
+  (let [shuffled (shuffle c)
+        player1  (take 26 shuffled)
+        player2  (drop 26 shuffled)]
+    {:player1 (vec player1)
+     :player2 (vec player2)}))
 
-(defn play-game [player1-cards player2-cards])
+(defn highest-rank
+  [card1 card2]
+  (let [rank1  (last card1)
+        rank2  (last card2)
+        index1 (.indexOf ranks rank1)
+        index2 (.indexOf ranks rank2)]
+    (cond
+      (> index1 index2) card1
+      (> index2 index1) card2
+      :else nil)))
+
+(defn highest-suit
+  [card1 card2]
+  (let [suit1  (first card1)
+        suit2  (first card2)
+        index1 (.indexOf suits suit1)
+        index2 (.indexOf suits suit2)]
+    (cond
+      (> index1 index2) card1
+      (> index2 index1) card2
+      :else nil)))
+
+(defn play-round [player1-card player2-card]
+  (or (highest-rank player1-card player2-card)
+      (highest-suit player1-card player2-card)))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [cards1 player1-cards
+         cards2 player2-cards]
+    (let [card1  (first cards1)
+          card2  (first cards2)
+          winner (play-round card1 card2)]
+      (if (= card1 winner)
+        (let [c1 (->> (-> cards1
+                          (conj card2)
+                          (conj card1))
+                      (drop 1)
+                      vec) 
+              c2 (vec (drop 1 cards2))]
+          (if (= 0 (count c2))
+            {:player1 c1
+             :player2 c2}
+            (recur c1 c2)))
+        (let [c2 (->> (-> cards2
+                          (conj card1)
+                          (conj card2))
+                      (drop 1)
+                      vec)
+              c1 (vec (drop 1 cards1))]
+          (if (= 0 (count c1))
+            {:player1 c1
+             :player2 c2}
+            (recur c1 c2)))))))
+
+(defn start-game []
+  (let [dealt-cards (deal-cards cards)]
+    (play-game (:player1 dealt-cards)
+               (:player2 dealt-cards))))
+
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,37 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (let [highest [:diamond 10]
+          lowest  [:heart 6]]
+      (is (= highest (play-round lowest highest)))))
+  (testing "queens are higher rank than jacks"
+    (let [queen [:diamond :queen]
+          jack  [:diamond :jack]]
+      (is (= queen (play-round queen jack)))))
+  (testing "kings are higher rank than queens"
+    (let [queen [:diamond :queen]
+          king  [:diamond :king]]
+      (is (= king (play-round queen king)))))
+  (testing "aces are higher rank than kings"
+    (let [king [:diamond :king]
+          ace  [:diamond :ace]]
+      (is (= ace (play-round ace king)))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (let [clubs  [:club 10]
+          spades [:spade 10]]
+      (is (= clubs (play-round spades clubs)))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (let [diamonds [:diamond 10]
+          clubs    [:club 10]]
+      (is (= diamonds (play-round diamonds clubs)))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (let [hearts   [:heart 10]
+          diamonds [:diamonds 10]]
+      (is (= hearts (play-round hearts diamonds))))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (let [game-result (start-game)]
+      (is (or (empty? (:player1 game-result))
+              (empty? (:player2 game-result)))))))
 
